7d7b99bef9d19b1d3f741b082141d5fd3e561899
KozlovOleg97 <kozlovoleg2021@gmail.com>
2023-03-02 16:56:15 +0300

    Add project files.

---
+++ ExplicitMemoryStruct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitStruct
+{
+	[StructLayoutAttribute(LayoutKind.Explicit)]
+	public struct ExplicitMemoryStruct
+	{
+		[FieldOffset(0)]
+		public int x;
+
+		[FieldOffset(0)] 
+		public int y;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExplicitStruct
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var explicitMemoryStruct = new ExplicitMemoryStruct();
+			explicitMemoryStruct.x = 7;
+			explicitMemoryStruct.y = 99;
+
+			Console.WriteLine(explicitMemoryStruct.x);
+			Console.ReadLine();
+		}
+	}
+}
